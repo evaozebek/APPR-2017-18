f2f7de14397a39441414a1a1c383fd7e50fe5963
Eva Ozebek <eva.ozebek@gmail.com>
2018-02-16 12:21:02 +0100

    Dopolnjeno poročilo

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -8,34 +8,22 @@ st.porok <- ggplot(osnovni.podatki.poroke %>% filter(spremenljivka == "Sklenitve
                    aes(x = leto, y = vrednost)) + geom_line() + 
   xlab("Leto") + ylab("Stevilo porok") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
- 
-#poskus - ZDRUŽITEV
+
+st.razvez <- ggplot(osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze - SKUPAJ"), 
+                    aes(x = leto, y = vrednost)) + geom_line() + 
+  xlab("Leto") + ylab("Stevilo razvez") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
 #========================================================================================================
+#ZDRUŽITEV
 st.porok1 <- ggplot() + geom_line(data = osnovni.podatki.poroke %>% filter(spremenljivka == "Sklenitve zakonskih zvez - Skupaj"),
                                                             aes(x = leto, y= vrednost), color="red") + 
   geom_line(data = osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze - SKUPAJ"), 
             aes(x = leto, y = vrednost), color="blue") +
   xlab("Leto") + ylab("Število") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-#manjka še legenda
+
 #========================================================================================================
-#######
-st.razvez <- ggplot(osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze - SKUPAJ"), 
-                    aes(x = leto, y = vrednost)) + geom_line() + 
-  xlab("Leto") + ylab("Stevilo razvez") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
-
-
-skupaj <- ggplot() + geom_line(aes(x = leto, y = vrednost)) + geom_line(aes(x = leto, y = vrednost))
-
-
-
-
-prve.zveze <-ggplot(osnovni.podatki.poroke %>% filter(spremenljivka == "Prve sklenitve zakonskih zvez"), 
-                                 aes(x = leto, y = vrednost)) + geom_line() + 
-  xlab("Leto") + ylab("Prve sklenitve zakonskih zvez") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 razveze.z.otroki <- ggplot(osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze zakonskih zvez z otroki"), 
                            aes(x = leto, y = vrednost)) + geom_line() + 
@@ -46,8 +34,27 @@ razveze.brez.otrok <- ggplot(osnovni.podatki.razveze %>% filter(spremenljivka ==
                            aes(x = leto, y = vrednost)) + geom_line() + 
   xlab("Leto") + ylab("Razveze zakonskih zvez brez otrok") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+#========================================================================================================
+#ZDRUŽITEV
+otroci <- ggplot() + geom_line(data = osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze zakonskih zvez z otroki"), 
+                               aes(x = leto, y = vrednost), color = "red") + 
+  geom_line(data = osnovni.podatki.razveze %>% filter(spremenljivka == "Razveze zakonskih zvez brez otrok"),
+            aes(x = leto, y = vrednost), color = "blue") +
+  xlab("Leto") + ylab("Število") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+#========================================================================================================
 
 
+prve.zveze <-ggplot(osnovni.podatki.poroke %>% filter(spremenljivka == "Prve sklenitve zakonskih zvez"), 
+                                 aes(x = leto, y = vrednost)) + geom_line() + 
+  xlab("Leto") + ylab("Prve sklenitve zakonskih zvez") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+trajanje <- ggplot(osnovni.podatki.razveze %>% filter(spremenljivka == "Trajanje zakonske zveze 5-9 let"), 
+                    aes(x = leto, y = vrednost)) + geom_line() + 
+  xlab("Leto") + ylab("Trajanje zakonske zveze 5-9 let") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 #še primerjava med zakonskim stanom ženina in neveste pred poroko(vdova,samski...)
 #========================================================================================================
