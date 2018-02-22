8307cfc2c037f541c93b03ec6f45e3173b7cd030
Eva Ozebek <eva.ozebek@gmail.com>
2018-02-22 10:59:10 +0100

    Minimalen popravek

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -14,10 +14,12 @@ osnovni.podatki <- rbind(osnovni.podatki.poroke, osnovni.podatki.razveze)
 
 ociscena <- subset(osnovni.podatki, spremenljivka == "Sklenitve zakonskih zvez - Skupaj")
 fit <- lm(data = ociscena, vrednost ~ leto)
-a <- data.frame(leto=seq(2016, 2030, 0.5))
+a <- data.frame(leto=seq(2016, 2030, 2))
 predict(fit, a)
 napoved <- a %>% mutate(vrednost=predict(fit, .))
 graf5 <- ggplot(ociscena, aes(x=leto, y=vrednost)) +
   geom_smooth(method=lm, se=FALSE, fullrange = TRUE) +
   geom_point(data=napoved, aes(x=leto, y=vrednost), color="orange", size=3) +
-  labs(title="Napoved števila porok", y="Število porok") + scale_x_continuous(limits = c(2017, 2030))
+  labs(title="Napoved števila porok", y="Število porok") + geom_point()
+
+#+ scale_x_continuous(limits = c(2017, 2030))
